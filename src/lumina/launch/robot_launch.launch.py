a03b7bec49c65f1dbadbb27ebb9618f0dfef99c3
Ryan Anderson <sonorousduck@gmail.com>
2025-01-11 13:52:32 -0700

    Added camera_frame_id

--- src/lumina/launch/robot_launch.launch.py
+++ src/lumina/launch/robot_launch.launch.py
@@ -9,23 +9,21 @@ def generate_launch_description():
             name='v4l2_camera_node',
             parameters=[{
                 'image_size': [640, 480],
+                "camera_frame_id": "camera_link_optical"
             }],
-            remappings=[
-                ('/image_raw', '/camera/image_raw')  # Raw image topic
-            ]
         ),
         
-        # Start an image transport node for compression
-        Node(
-            package='image_transport',
-            executable='republish',
-            name='image_compressor',
-            arguments=['raw', 'compressed'],
-            remappings=[
-                ('in', '/camera/image_raw'),        # Input raw image
-                ('out/compressed', '/camera/image_compressed')  # Output compressed image
-            ]
-        ),
+        # # Start an image transport node for compression
+        # Node(
+        #     package='image_transport',
+        #     executable='republish',
+        #     name='image_compressor',
+        #     arguments=['raw', 'compressed'],
+        #     remappings=[
+        #         ('in', '/camera/image_raw'),        # Input raw image
+        #         ('out/compressed', '/camera/image_compressed')  # Output compressed image
+        #     ]
+        # ),
         
         # Start the Lidar
         Node(
