a1977a44b91e3394409f3c2f2831b6a77dc60279
Ryan Anderson <sonorousduck@gmail.com>
2025-01-12 06:51:48 -0700

    Working on updating to the ign version of gazebo since the classic version wasn't working well

--- src/lumina/launch/launch_sim.launch.py
+++ src/lumina/launch/launch_sim.launch.py
@@ -1,47 +1,82 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
-
-
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
 
+    # Set the package name
+    package_name = 'lumina'
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='lumina'
-    
+    # Include the robot_state_publisher launch file
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+        )]), 
+        launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+    # Gazebo parameters file
+    default_world = os.path.join(
+        get_package_share_directory(package_name),
+        'worlds',
+        'empty.world'
+        ) 
+
+    world = LaunchConfiguration('world')
+
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=default_world,
+        description='World to load'
+        )
+
+    # Include the Gazebo launch file and pass the world file argument
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    get_package_share_directory('ros_ign_gazebo'), 'launch', 'ign_gazebo.launch.py')]),
+                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
              )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+    # Run the spawner node from the gazebo_ros package to spawn the entity
+    spawn_entity = Node(package='ros_ign_gazebo', executable='create',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'lumina'],
+                                   '-name', 'lumina',
+                                   '-z', '0.1'],
                         output='screen')
+    
+    bridge_params = os.path.join(get_package_share_directory(package_name),'config','ign_bridge.yaml')
+    ros_gz_bridge = Node(
+        package="ros_ign_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
+    )
+    
+    # ros_gz_image_bridge = Node(
+    #     package="ros_ign_image",
+    #     executable="image_bridge",
+    #     arguments=["/camera/image_raw"]
+    # )
 
 
-
-    # Launch them all!
+    # Launch all components together
     return LaunchDescription([
         rsp,
+        # joystick,
+        # twist_mux,
+        world_arg,
         gazebo,
         spawn_entity,
+        # diff_drive_spawner,
+        # joint_broad_spawner,
+        ros_gz_bridge,
+        # ros_gz_image_bridge
     ])
--- src/lumina/setup.py
+++ src/lumina/setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
         (os.path.join('share', package_name, 'description'), glob(os.path.join('description', '*.xacro'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
         ],
     install_requires=['setuptools'],
     zip_safe=True,
