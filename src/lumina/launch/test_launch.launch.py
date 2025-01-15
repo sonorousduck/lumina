7085353082e04883fe7e351b938d901c6481bc9d
Ryan Anderson <sonorousduck@gmail.com>
2025-01-15 08:47:48 -0700

    Got the test launch working! Need to convert it to the robot launch so I have all the sensors as well

--- src/lumina/launch/test_launch.launch.py
+++ src/lumina/launch/test_launch.launch.py
@@ -131,11 +131,7 @@ def generate_launch_description():
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments = [
-            "diffbot_base_controller",
-            "--controller_manager",
-            "/controller_manager"
-        ],
+        arguments=["diffbot_base_controller", "--controller-manager", "/controller_manager"],
     )
     
     # Delay rviz start until after `joint_state_broadcaster`
